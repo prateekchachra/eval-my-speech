f450d241a5d865337641ff4c5142a78d37d72322
Prateek Chachra <prateekchachra@live.com>
2019-06-23 01:57:37 +0530

    Removed redux extension to resolve bug on browsers

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,9 @@
 
-import {createStore, applyMiddleware, compose} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 const initialState = {};
 
-const middleware = [thunk];
-const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware),
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-);
+const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
 
 export default store;
